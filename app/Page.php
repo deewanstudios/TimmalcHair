17c2b7afedce46ab2c63e8b3beda06c82602100e
Adedayo Adedapo <daniel@deewanstudios.com>
2020-01-27 19:15:35 +0000

    # Changes
    	* Added fillable property to page model, to allow for the
    	insertion of data into table columns.
    
    	** Added path metho to page class
    
    	*** Deleted products' migration, model and factory. This is to
    	prevent foreign key constraint error, as all other tables that
    	this table will relate to are yet to be built.

--- app/Page.php
+++ app/Page.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'title',
+        'description',
+        'keywords',
+        'url',
+        'visibility',
+    ];
+
+    public function path()
+    {
+        return $this->url;
+    }
 }
